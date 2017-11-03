5ef7a377a4ac3ff82efdb7984a65b2d3fe3d5928
Peter van Hardenberg <pvh@pvh.ca>
2017-11-03 12:43:57 -0700

    move patch-considering code into submission / untangle checking submission logic a bit

--- cfbot/__init__.py
+++ cfbot/__init__.py
@@ -1,12 +1,9 @@
 import gzip
-import HTMLParser
 import unicodedata
-import urllib
-import urllib2
-import urlparse
 
 from prep import *
 from submission import *
+from commitfest import *
 
 # travis build settings that will be added to automatically generated branches
 TRAVIS_FILE = """
@@ -58,75 +55,6 @@ after_failure:
     done
 """
 
-
-def get_thread_url_for_submission(commitfest_id, submission_id):
-  """Given a commitfest ID and a submission ID, return the URL of the 'whole
-     thread' page in the mailing list archives."""
-  if submission_id == 951:
-    # this one has two threads, and the interesting one is listed first (need to learn about dates?)
-    return "https://www.postgresql.org/message-id/flat/CAEepm=1iiEzCVLD=RoBgtZSyEY1CR-Et7fRc9prCZ9MuTz3pWg@mail.gmail.com"
-  elif submission_id == 994:
-    # this one is truncated, and there is a new 'flat' URL for the continuation
-    return "https://www.postgresql.org/message-id/flat/CAOGQiiN9m%3DKRf-et1T0AcimbyAB9hDzJqGkHnOBjWT4uF1z1BQ%40mail.gmail.com"
-  # if there is more than one, we'll take the furthest down on the page...
-  result = None
-  url = "https://commitfest.postgresql.org/%s/%s/" % (commitfest_id, submission_id)
-  for line in slow_fetch(url).splitlines():
-    groups = re.search('<dt><a href="(https://www.postgresql.org/message-id/flat/[^"]+)"', line)
-    if groups:
-      result = groups.group(1)
-  return result
-  
-def get_submissions_for_commitfest(commitfest_id):
-  """Given a commitfest ID, return a list of Submission objects."""
-  result = []
-  parser = HTMLParser.HTMLParser()
-  url = "https://commitfest.postgresql.org/%s/" % (commitfest_id,)
-  next_line_has_authors = False
-  state = None
-  for line in slow_fetch(url).splitlines():
-    groups = re.search('\<a href="([0-9]+)/"\>([^<]+)</a>', line)
-    if groups:
-      submission_id = groups.group(1)
-      name = parser.unescape(groups.group(2))
-    if next_line_has_authors:
-      next_line_has_authors = False
-      groups = re.search("<td>([^<]*)</td>", line)
-      if groups:
-        authors = groups.group(1)
-        authors = re.sub(" *\\([^)]*\\)", "", authors)
-        result.append(Submission(submission_id, commitfest_id, name, state, authors))
-        continue
-    groups = re.search('<td><span class="label label-[^"]*">([^<]+)</span></td>', line)
-    if groups:
-      state = groups.group(1)
-      next_line_has_authors = True
-      continue
-    next_line_has_authors = False
-  return result
-
-def get_current_commitfest_id():
-  """Find the ID of the current open or next future commitfest."""
-  result = None
-  for line in slow_fetch("https://commitfest.postgresql.org").splitlines():
-    groups = re.search('<a href="/([0-9]+)/">[0-9]+-[0-9]+</a> \((Open|In Progress) ', line)
-    if groups:
-      commitfest_id = groups.group(1)
-      state = groups.group(2)
-      result = int(commitfest_id)
-  return result
-
-def read_file(path):
-  """Return the contents of file 'path'."""
-  with open(path) as f:
-    return f.read()
-
-def write_file(path, data):
-  """Write 'data' into 'path' atomically."""
-  with open(path + ".tmp", "w+") as f:
-    f.write(data)
-  os.rename(path + ".tmp", path)
-
 def sort_and_rotate_submissions(log, submissions):
   """Sort the given list of submissions, and then rotate them so that the one
      that follows 'last_submission_id' comes first (unless it is None).  This
@@ -161,144 +89,13 @@ def check_n_submissions(log, commit_id, submissions, n):
 
   # now process n submissions, starting after that one
   for submission in sort_and_rotate_submissions(log, submissions):
-    log.write("==> considering submission ID %s\n" % submission.id)
-    log.flush()
-    patch_dir = os.path.join("patches", str(submission.commitfest_id), str(submission.id))
-    if os.path.isdir(patch_dir):
-      # write name and status to disk so our web page builder can use them...
-      write_file(os.path.join(patch_dir, "status"), submission.status)
-      write_file(os.path.join(patch_dir, "name"), submission.name)
-    thread_url = get_thread_url_for_submission(submission.commitfest_id, submission.id)
-    #if submission.status not in ("Ready for Committer", "Needs review"):
-    #  continue
-    if thread_url == None:
-      continue
+    activity_message = submission.check(log, commit_id)
+    n = n - 1
+    # remember this ID so we can start after this next time
+    write_last_submission_id(submission)
 
-    new_patch = False
-    message_id, patches = get_latest_patches_from_thread_url(thread_url)
-    if message_id:
-      # download the patches, if we don't already have them
-      message_id_path = os.path.join(patch_dir, "message_id")
-      if not os.path.exists(message_id_path) or read_file(message_id_path) != message_id:
-        new_patch = True # affects the friendly status message
-        log.write("    message ID %s is new\n" % message_id)
-        log.flush()
-        tmp = patch_dir + ".tmp"
-        if os.path.exists(tmp):
-          shutil.rmtree(tmp)
-        if os.path.exists(patch_dir):
-          shutil.rmtree(patch_dir)
-        os.mkdir(tmp)
-        for patch in patches:
-          parsed = urlparse.urlparse(patch)
-          filename = os.path.basename(parsed.path)
-          dest = os.path.join(tmp, filename)
-          log.write("    fetching patch %s\n" % patch)
-          log.flush()
-          urllib.urlretrieve(patch, dest)
-          time.sleep(SLOW_FETCH_SLEEP)
-        write_file(os.path.join(tmp, "message_id"), message_id)
-        write_file(os.path.join(tmp, "status"), submission.status)
-        write_file(os.path.join(tmp, "name"), submission.name)
-        os.rename(tmp, patch_dir)
-
-      # if the commit ID has moved since last time, or we
-      # have a new patchest, then we need to make a new branch
-      # to trigger a new build
-      commit_id_path = os.path.join("patches", str(submission.commitfest_id), str(submission.id), "commit_id")
-      if not os.path.exists(commit_id_path) or read_file(commit_id_path) != commit_id:
-        log.write("    commit ID %s is new\n" % commit_id)
-        log.flush()
-        branch = "commitfest/%s/%s" % (submission.commitfest_id, submission.id)
-        subprocess.check_call("cd postgresql && git checkout . > /dev/null && git clean -fd > /dev/null && git checkout -q master", shell=True)
-        failed_to_apply = False
-        with open(os.path.join("logs", str(submission.commitfest_id), str(submission.id) + ".log"), "w") as apply_log:
-          apply_log.write("== Fetched patches from message ID %s\n" % message_id)
-          apply_log.write("== Applying on top of commit %s\n" % commit_id)
-          for path in sorted(os.listdir(patch_dir)):
-            if path.endswith(".patch"):
-              with open(os.path.join(patch_dir, path), "r") as f:
-                apply_log.write("== Applying patch %s...\n" % path)
-                apply_log.flush()
-                popen = subprocess.Popen("cd postgresql && patch -p1 --no-backup-if-mismatch --batch --silent", shell=True, stdin=f, stdout=apply_log, stderr=apply_log)
-                popen.wait()
-                if popen.returncode != 0:
-                  failed_to_apply = True
-                  break
-            elif path.endswith(".patch.gz"):
-              with gzip.open(os.path.join(patch_dir, path), "r") as f:
-                apply_log.write("== Applying patch %s...\n" % path)
-                apply_log.flush()
-                popen = subprocess.Popen("cd postgresql && patch -p1 --no-backup-if-mismatch --batch --silent", shell=True, stdin=subprocess.PIPE, stdout=apply_log, stderr=apply_log)
-                popen.communicate(input=f.read())
-                popen.wait()
-                if popen.returncode != 0:
-                  failed_to_apply = True
-                  break
-            elif path.endswith(".tgz") or path.endswith(".tar.gz") or path.endswith(".tar.bz2"):
-              apply_log.write("== Applying patches from tarball %s...\n" % path)
-              apply_log.flush()
-              # TODO catch errors manipulating tar files...
-              with tarfile.open(os.path.join(patch_dir, path), "r") as tarball:
-                for name in sorted(tarball.getnames()):
-                  if not name.endswith(".patch") or "/." in name:
-                    continue
-                  apply_log.write("== Applying patch %s...\n" % name)
-                  apply_log.flush()
-                  popen = subprocess.Popen("cd postgresql && patch -p1 --no-backup-if-mismatch --batch --silent", shell=True, stdin=subprocess.PIPE, stdout=apply_log, stderr=apply_log)
-                  f = tarball.extractfile(name)
-                  popen.communicate(input=f.read())
-                  f.close()
-                  popen.wait()
-                  if popen.returncode != 0:
-                    failed_to_apply = True
-                    break
-        apply_status_path = os.path.join("patches", str(submission.commitfest_id), str(submission.id), "apply_status")
-        if failed_to_apply:
-          log.write("    apply failed (see apply log for details)\n")
-          log.flush()
-          write_file(apply_status_path, "failing")
-          # no point in trying again until either the message ID or the commit ID moves
-          write_file(commit_id_path, commit_id)
-        else:
-          write_file(apply_status_path, "passing")
-          write_file("postgresql/.travis.yml", TRAVIS_FILE)
-          subprocess.call("cd postgresql && git branch -q -D %s > /dev/null 2> /dev/null" % (branch,), shell=True) # ignore if fail
-          subprocess.check_call("cd postgresql && git checkout -q -b %s" % (branch,), shell=True)
-          subprocess.check_call("cd postgresql && git add -A", shell=True)
-          log.write("    creating new branch %s\n" % branch)
-          log.flush()
-          write_file("commit_message", """[CF %s/%s] %s
-
-This commit was automatically generated by cfbot at commitfest.cputube.org.
-It is based on patches submitted to the PostgreSQL mailing lists and
-registered in the PostgreSQL Commitfest application.
-
-This branch will be overwritten each time a new patch version is posted to
-the email thread or the master branch changes.
-
-Commitfest entry: https://commitfest.postgresql.org/%s/%s
-Patch(es): https://www.postgresql.org/message-id/%s
-Author(s): %s
-""" % (submission.commitfest_id, submission.id, submission.name, submission.commitfest_id, submission.id, message_id, submission.authors))
-          subprocess.check_call("cd postgresql && git commit -q -F ../commit_message", shell=True)
-          write_file(commit_id_path, commit_id)
-          if False: # disable pushing for my own testing purposes
-            log.write("    pushing branch %s\n" % branch)
-            log.flush()
-            os.environ["GIT_SSH_COMMAND"] = CFBOT_REPO_SSH_COMMAND
-            subprocess.check_call("cd postgresql && git push -q -f cfbot-repo %s" % (branch,), shell=True)
-            if new_patch:
-              activity_message = """Pushed branch <a href="https://github.com/postgresql-cfbot/postgresql/tree/%s">%s</a>, triggered by <a href="https://www.postgresql.org/message-id/%s">new patch</a>.""" % (branch, branch, message_id)
-            else:
-              activity_message = """Pushed branch <a href="https://github.com/postgresql-cfbot/postgresql/tree/%s">%s</a>, triggered by commit <a href="https://git.postgresql.org/gitweb/?p=postgresql.git;a=commitdiff;h=%s">%s</a>.  Waiting for a while to be polite before rebuilding items marked "&bull;"...""" % (branch, branch, commit_id, commit_id[:8])
-          n = n - 1
-
-      # remember this ID so we can start after this next time
-      write_last_submission_id(submission)
-
-      if n <= 0:
-        break
+    if n <= 0:
+      break
   return activity_message
 
 def make_author_url(author):
@@ -311,53 +108,6 @@ def make_author_url(author):
     text = re.sub('[ ]+', '-', text)
     text = re.sub('[^0-9a-zA-Z_-]', '', text)
     return text + ".html"
-
-
-def slow_fetch(url):
-  """Fetch the body of a web URL, but sleep every time too to be kind to the
-     commitfest server."""
-  opener = urllib2.build_opener()
-  opener.addheaders = [('User-Agent', USER_AGENT)]
-  response = opener.open(url)
-  body = response.read()
-  response.close()
-  time.sleep(SLOW_FETCH_SLEEP)
-  return body
-  
-def get_latest_patches_from_thread_url(thread_url):
-  """Given a 'whole thread' URL from the archives, find the last message that
-     had at least one attachment called something.patch.  Return the message
-     ID and the list of URLs to fetch all the patches."""
-  selected_message_attachments = []
-  selected_message_id = None
-  message_attachments = []
-  message_id = None
-  for line in slow_fetch(thread_url).splitlines():
-    groups = re.search('<a href="(/message-id/attachment/[^"]*\\.(patch|patch\\.gz|tar\\.gz|tgz|tar\\.bz2))">', line)
-    if groups:
-      message_attachments.append("https://www.postgresql.org" + groups.group(1))
-      selected_message_attachments = message_attachments
-      selected_message_id = message_id
-    else:
-      groups = re.search('<a name="([^"]+)"></a>', line)
-      if groups:
-        message_id = groups.group(1)
-        message_attachments = []
-  # if there is a tarball attachment, there must be only one attachment,
-  # otherwise give up on this thread (we don't know how to combine patches and
-  # tarballs)
-  if selected_message_attachments != None:
-    if any(x.endswith(".tgz") or x.endswith(".tar.gz") or x.endswith(".tar.bz2") for x in selected_message_attachments):
-      if len(selected_message_attachments) > 1:
-        selected_message_id = None
-        selected_message_attachments = None
-  # if there are multiple patch files, they had better follow the convention
-  # of leading numbers, otherwise we don't know how to apply them in the right
-  # order
-  # TODO
-  return selected_message_id, selected_message_attachments
-
-
  
 def build_web_page(commit_id, commitfest_id, submissions, filter_author, activity_message, path):
   """Build a web page that lists all known entries and shows the badges."""
--- run.py
+++ run.py
@@ -10,23 +10,33 @@ def run(num_branches_to_push):
     return
   prepare_repo()
   commit_id = update_tree()
-  commitfest_id = get_current_commitfest_id()
-  prepare_filesystem(commitfest_id)
-  prepare_filesystem(commitfest_id + 1)
-  submissions = get_submissions_for_commitfest(commitfest_id) + get_submissions_for_commitfest(commitfest_id + 1)
+  
+  # fetch the current and next commitfests
+  commitfest = get_current_commitfest()
+  next_commitfest = CommitFest(commitfest.id + 1)
+
+  # prep both filesystems
+  prepare_filesystem(commitfest.id)
+  prepare_filesystem(next_commitfest.id)
+
+  # now consider all the submissions for both as a single array
+  submissions = commitfest.get_submissions() + next_commitfest.get_submissions()
+
+  # reduce these to just "interesting" statuses
   submissions = filter(lambda s: s.status in ("Ready for Committer", "Needs review", "Waiting on Author"), submissions)
+  
   with open("logs/cfbot.%s.log" % datetime.date.today().isoformat(), "a") as log:
     log.write("== starting at %s\n" % str(datetime.datetime.now()))
-    log.write("commitfest = %s\n" % commitfest_id)
+    log.write("commitfest = %s\n" % commitfest.id)
     log.write("commit = %s\n" % commit_id)
     log.flush()
     activity_message = check_n_submissions(log, commit_id, submissions, num_branches_to_push)
     log.write("== finishing at %s\n" % str(datetime.datetime.now()))
     log.flush()
-  build_web_page(commit_id, commitfest_id, submissions, None, activity_message, "www/index.html")
+  build_web_page(commit_id, commitfest.id, submissions, None, activity_message, "www/index.html")
   for author in unique_authors(submissions):
     build_web_page(commit_id, None, submissions, author, activity_message, "www/" + make_author_url(author))
-  build_web_page(commit_id, commitfest_id + 1, submissions, None, activity_message, "www/next.html")
+  build_web_page(commit_id, next_commitfest.id, submissions, None, activity_message, "www/next.html")
   lock.close()
 
 if __name__ == "__main__":
