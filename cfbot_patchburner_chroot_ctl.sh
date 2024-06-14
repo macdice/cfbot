a94661c99d2e78d31c7ef2f51a2497c1a8ad72ff
Jelte Fennema-Nio <jelte.fennema@microsoft.com>
2024-06-14 19:24:30 +0200

    Don't squash patches into one big commit
    As I explained in #17 the cfbot repo is the easiest way for me to take a
    quick look at postgres patches in a decent diff interface, as well as
    having an easy way of checking them out locally. Instead of squashing
    the patches together it's much nicer if they are committed separately.
    
    The suggested changes `git mailinfo` to extract to extract commit info
    in case the patch was created by `git format-patch`. It explicitely
    doesn't use `git am` to apply the patch, because that chokes on many
    more merge conflicts as regular `patch`/`gpatch` does.
    
    P.S. This starts using <<'EOF' instead of <<EOF to not have to escape
    dollar signs all the time in the `apply-patches.sh` script.

--- cfbot_patchburner_chroot_ctl.sh
+++ cfbot_patchburner_chroot_ctl.sh
@@ -73,10 +73,10 @@ create_patchburner()
       for bin in sh dash ; do
         cp /bin/$bin $CHROOT_DIR//bin/
       done
-      for bin in patch unzip gzip gunzip tar find sort ; do
+      for bin in cat patch unzip git gzip gunzip sed tar find sort ; do
         cp /usr/bin/$bin $CHROOT_DIR/usr/bin/
       done
-      for lib in libbz2.so libacl.so libselinux.so libc.so libm.so libattr.so libpcre.so libpcre2-8.so libdl.so libpthread.so ; do
+      for lib in libbz2.so libacl.so libselinux.so libc.so libm.so libattr.so libpcre.so libpcre2-8.so libdl.so libpthread.so libz.so; do
         cp /lib/x86_64-linux-gnu/$lib* $CHROOT_DIR/lib/x86_64-linux-gnu/
       done
       cp /lib64/ld-linux-x86-64.so* $CHROOT_DIR/lib64/
@@ -87,9 +87,15 @@ create_patchburner()
       ;;
   esac
 
+  # "git sendmail" uses /dev/null internally, so we need to have a /dev
+  # directory. We clean the file up afterwards, so it's not a big deal that
+  # it's not actually a special device
+  mkdir $CHROOT_DIR/dev
+  touch $CHROOT_DIR/dev/null
+
   # create the patching script
   mkdir $CHROOT_DIR/work/patches
-  cat > $CHROOT_DIR/work/apply-patches.sh <<EOF
+  cat > $CHROOT_DIR/work/apply-patches.sh <<'EOF'
 #!/bin/sh
 #
 # apply all patches found in /work/patches
@@ -98,25 +104,47 @@ set -e
 
 # unpack and zip archives, tarballs etc
 cd /work/patches
-for f in \$(find . -name '*.tgz' -o -name '*.tar.gz' -o -name '*.tar.bz2') ; do
-  echo "=== expanding \$f"
-  tar xzvf \$f
+for f in $(find . -name '*.tgz' -o -name '*.tar.gz' -o -name '*.tar.bz2') ; do
+  echo "=== expanding $f"
+  tar xzvf $f
 done
-for f in \$(find . -name '*.gz') ; do
-  echo "=== expanding \$f"
-  gunzip \$f
+for f in $(find . -name '*.gz') ; do
+  echo "=== expanding $f"
+  gunzip $f
 done
-for f in \$(find . -name '*.zip') ; do
-  echo "=== expanding \$f"
-  unzip \$f
+for f in $(find . -name '*.zip') ; do
+  echo "=== expanding $f"
+  unzip $f
 done
 
 # now apply all .patch and .diff files
 cd /work/postgresql
-for f in \$(cd /work/patches && find . -name '*.patch' -o -name '*.diff' | sort) ; do
-  echo "=== applying patch \$f"
-  patch --no-backup-if-mismatch -p1 -V none -f < "/work/patches/\$f"
+
+# But first set up the git user
+git config user.name "Commitfest Bot"
+git config user.email "cfbot@cputube.org"
+
+for f in $(cd /work/patches && find . -name '*.patch' -o -name '*.diff' | sort) ; do
+  echo "=== applying patch $f"
+  git mailinfo ../msg ../patch < "/work/patches/$f" > ../info
+  # Clean out /dev/null in case git mailinfo wrote something to it
+  : > /dev/null
+
+  NAME=$(sed -n -e 's/^Author: //p' ../info)
+  EMAIL=$(sed -n -e 's/^Email: //p' ../info)
+  SUBJECT=$(sed -n -e 's/^Subject: //p' ../info)
+  DATE=$(sed -n -e 's/^Date: //p' ../info)
+  MESSAGE="$(cat ../msg)"
+  MESSAGE="${SUBJECT:-"[PATCH]: $f"}${MESSAGE:+
+
+}${MESSAGE}"
+
+  patch --no-backup-if-mismatch -p1 -V none -f < "/work/patches/$f"
+  git add .
+  git commit -m "$MESSAGE" --author="${NAME:-Commitfest Bot} <${EMAIL:-cfbot@cputube.org}>" --date="${DATE:-now}"
+
 done
+
 EOF
   chmod 775 $CHROOT_DIR/work/apply-patches.sh
   chown -R $RUNAS_USER:$RUNAS_USER $CHROOT_DIR/work
