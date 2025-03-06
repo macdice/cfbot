1b67405f4dd90e052f27aaf72501856b20cf1e19
Jelte Fennema-Nio <github-tech@jeltef.nl>
2025-03-07 00:33:09 +0100

    Format cfbot_patchburner_ctl.sh using shfmt

--- cfbot_patchburner_ctl.sh
+++ cfbot_patchburner_ctl.sh
@@ -5,7 +5,7 @@
 #
 # CFBOT_USER must already exist on the host.  The guest will have a user with
 # the same UID for ease of access to the jailed filesystem.
-# 
+#
 # Requirements: ezjail, sudo, git installed, then:
 #
 # /etc/rc.conf:
@@ -34,56 +34,52 @@ HOST_ROOT_PATH=/usr/jails/$JAIL_NAME
 CFBOT_USER=cfbot
 CFBOT_UID=1002
 
-usage()
-{
-  echo "Usage: $1 init|create|apply|destroy"
-  echo
-  echo "init-template -- create a new jail 'patchbase'"
-  echo
-  echo "create -- create a new jail 'patchburner'"
-  echo "apply -- apply all the patches found in /work/patches'"
-  echo "destroy -- destroy 'patchburner' if it exists"
-  echo
-  echo "template-repo-patch -- report path of template git repo"
-  echo "burner-patch-path -- report path where patches should be placed"
-  echo "burner-repo-path -- report path of burner git repo"
-  exit 1
+usage() {
+	echo "Usage: $1 init|create|apply|destroy"
+	echo
+	echo "init-template -- create a new jail 'patchbase'"
+	echo
+	echo "create -- create a new jail 'patchburner'"
+	echo "apply -- apply all the patches found in /work/patches'"
+	echo "destroy -- destroy 'patchburner' if it exists"
+	echo
+	echo "template-repo-patch -- report path of template git repo"
+	echo "burner-patch-path -- report path where patches should be placed"
+	echo "burner-repo-path -- report path of burner git repo"
+	exit 1
 }
 
-init_template()
-{
-  ezjail-admin create $TEMPLATE_JAIL_NAME 'lo2|127.0.1.0'
-  ezjail-admin start $TEMPLATE_JAIL_NAME
-  jexec $TEMPLATE_JAIL_NAME pw useradd -n $CFBOT_USER -u $CFBOT_UID
-  ezjail-admin stop $TEMPLATE_JAIL_NAME
+init_template() {
+	ezjail-admin create $TEMPLATE_JAIL_NAME 'lo2|127.0.1.0'
+	ezjail-admin start $TEMPLATE_JAIL_NAME
+	jexec $TEMPLATE_JAIL_NAME pw useradd -n $CFBOT_USER -u $CFBOT_UID
+	ezjail-admin stop $TEMPLATE_JAIL_NAME
 
-  mkdir $TEMPLATE_HOST_ROOT_PATH/work
-  chown $CFBOT_USER:$CFBOT_USER $TEMPLATE_HOST_ROOT_PATH/work
-  sudo -u $CFBOT_USER git clone git://git.postgresql.org/git/postgresql.git $TEMPLATE_HOST_ROOT_PATH/work/postgresql
+	mkdir $TEMPLATE_HOST_ROOT_PATH/work
+	chown $CFBOT_USER:$CFBOT_USER $TEMPLATE_HOST_ROOT_PATH/work
+	sudo -u $CFBOT_USER git clone git://git.postgresql.org/git/postgresql.git $TEMPLATE_HOST_ROOT_PATH/work/postgresql
 }
 
-destroy_patchburner_if_exists()
-{
-  if ezjail-admin list | grep " $JAIL_NAME " > /dev/null ; then
-    ezjail-admin delete -f $JAIL_NAME
-  fi
-  if zfs list $ZFS_NAME > /dev/null 2>&1 ; then
-    zfs destroy -f $ZFS_NAME
-  fi
-  if zfs list $TEMPLATE_ZFS_NAME@mysnapshot > /dev/null 2>&1 ; then
-    zfs destroy -f $TEMPLATE_ZFS_NAME@mysnapshot
-  fi
+destroy_patchburner_if_exists() {
+	if ezjail-admin list | grep " $JAIL_NAME " >/dev/null; then
+		ezjail-admin delete -f $JAIL_NAME
+	fi
+	if zfs list $ZFS_NAME >/dev/null 2>&1; then
+		zfs destroy -f $ZFS_NAME
+	fi
+	if zfs list $TEMPLATE_ZFS_NAME@mysnapshot >/dev/null 2>&1; then
+		zfs destroy -f $TEMPLATE_ZFS_NAME@mysnapshot
+	fi
 }
 
-create_patchburner()
-{
-  # clone it
-  zfs snapshot $TEMPLATE_ZFS_NAME@mysnapshot
-  zfs clone    $TEMPLATE_ZFS_NAME@mysnapshot $ZFS_NAME
-  ezjail-admin create -x $JAIL_NAME 'lo2|127.0.1.2'
-  mkdir $HOST_ROOT_PATH/work/patches
-  chown $CFBOT_USER:$CFBOT_USER $HOST_ROOT_PATH/work/patches
-  cat > $HOST_ROOT_PATH/work/apply-patches.sh <<'EOF'
+create_patchburner() {
+	# clone it
+	zfs snapshot $TEMPLATE_ZFS_NAME@mysnapshot
+	zfs clone $TEMPLATE_ZFS_NAME@mysnapshot $ZFS_NAME
+	ezjail-admin create -x $JAIL_NAME 'lo2|127.0.1.2'
+	mkdir $HOST_ROOT_PATH/work/patches
+	chown $CFBOT_USER:$CFBOT_USER $HOST_ROOT_PATH/work/patches
+	cat >$HOST_ROOT_PATH/work/apply-patches.sh <<'EOF'
 #!/bin/sh
 #
 # apply all patches found in /work/patches
@@ -141,45 +137,43 @@ for f in $(cd /work/patches && find . -name '*.patch' -o -name '*.diff' | sort) 
 
 done
 EOF
-  chmod 775 $HOST_ROOT_PATH/work/apply-patches.sh
+	chmod 775 $HOST_ROOT_PATH/work/apply-patches.sh
 }
 
-apply_patches_in_patchburner()
-{
-  ezjail-admin start $JAIL_NAME > /dev/null
-  jexec -U $CFBOT_USER $JAIL_NAME /work/apply-patches.sh
-  result=$?
-  ezjail-admin stop $JAIL_NAME > /dev/null
-  rm -rf $HOST_ROOT_PATH/work/postgresql/.git/hooks
-  rm -rf $HOST_ROOT_PATH/work/postgresql/.git/config
-  cp $TEMPLATE_HOST_ROOT_PATH/work/postgresql/.git/config $HOST_ROOT_PATH/work/postgresql/.git/config
-  exit $result
+apply_patches_in_patchburner() {
+	ezjail-admin start $JAIL_NAME >/dev/null
+	jexec -U $CFBOT_USER $JAIL_NAME /work/apply-patches.sh
+	result=$?
+	ezjail-admin stop $JAIL_NAME >/dev/null
+	rm -rf $HOST_ROOT_PATH/work/postgresql/.git/hooks
+	rm -rf $HOST_ROOT_PATH/work/postgresql/.git/config
+	cp $TEMPLATE_HOST_ROOT_PATH/work/postgresql/.git/config $HOST_ROOT_PATH/work/postgresql/.git/config
+	exit $result
 }
 
 case $1 in
-  init-template)
-    init_template
-    ;;
-  create)
-    create_patchburner
+init-template)
+	init_template
 	;;
-  destroy)
-    destroy_patchburner_if_exists
-    ;;
-  apply)
-    apply_patches_in_patchburner
-    ;;
-  template-repo-path)
-    echo $TEMPLATE_HOST_ROOT_PATH/work/postgresql
-    ;;
-  burner-patch-path)
-    echo $HOST_ROOT_PATH/work/patches
-    ;;
-  burner-repo-path)
-    echo $HOST_ROOT_PATH/work/postgresql
-    ;;
-  *)
-    usage
-    ;;
+create)
+	create_patchburner
+	;;
+destroy)
+	destroy_patchburner_if_exists
+	;;
+apply)
+	apply_patches_in_patchburner
+	;;
+template-repo-path)
+	echo $TEMPLATE_HOST_ROOT_PATH/work/postgresql
+	;;
+burner-patch-path)
+	echo $HOST_ROOT_PATH/work/patches
+	;;
+burner-repo-path)
+	echo $HOST_ROOT_PATH/work/postgresql
+	;;
+*)
+	usage
+	;;
 esac
-
