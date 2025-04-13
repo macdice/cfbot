4fa21b3764c140abc34b94014f554af73e46502f
Jelte Fennema-Nio <github-tech@jeltef.nl>
2025-04-13 16:03:16 +0200

    shfmt the docker/podman patchburner scripts

--- cfbot_patchburner_podman_ctl.sh
+++ cfbot_patchburner_podman_ctl.sh
@@ -7,79 +7,74 @@ set -e
 TEMPLATE_DIR=patchburner_template
 MOUNTED_DIR=patchburner_podman
 
-usage()
-{
-  echo "Usage: $1 init|create|apply|destroy"
-  echo
-  echo "init-template -- create 'patchburner_template'"
-  echo
-  echo "create -- create a new chroot under 'patchburner'"
-  echo "apply -- apply all the patches found in patchburner/work/patches'"
-  echo "destroy -- destroy 'patchburner' if it exists"
-  echo
-  echo "template-repo-patch -- report path of template git repo"
-  echo "burner-patch-path -- report path where patches should be placed"
-  echo "burner-repo-path -- report path of burner git repo"
-  exit 1
+usage() {
+	echo "Usage: $1 init|create|apply|destroy"
+	echo
+	echo "init-template -- create 'patchburner_template'"
+	echo
+	echo "create -- create a new chroot under 'patchburner'"
+	echo "apply -- apply all the patches found in patchburner/work/patches'"
+	echo "destroy -- destroy 'patchburner' if it exists"
+	echo
+	echo "template-repo-patch -- report path of template git repo"
+	echo "burner-patch-path -- report path where patches should be placed"
+	echo "burner-repo-path -- report path of burner git repo"
+	exit 1
 }
 
-init_template()
-{
-  # This is just a clean checkout of the git repo, which cfbot will keep
-  # updated, and we'll copy whever we need a throw-away copy to apply patches
-  # to.  This just avoids having to clone it every time, which would suck.
-  # You should only need to init once.
-  mkdir $TEMPLATE_DIR
-  mkdir $TEMPLATE_DIR/work
-  git clone git://git.postgresql.org/git/postgresql.git $TEMPLATE_DIR/work/postgresql
+init_template() {
+	# This is just a clean checkout of the git repo, which cfbot will keep
+	# updated, and we'll copy whever we need a throw-away copy to apply patches
+	# to.  This just avoids having to clone it every time, which would suck.
+	# You should only need to init once.
+	mkdir $TEMPLATE_DIR
+	mkdir $TEMPLATE_DIR/work
+	git clone git://git.postgresql.org/git/postgresql.git $TEMPLATE_DIR/work/postgresql
 }
 
-destroy_patchburner_if_exists()
-{
-  rm -fr $MOUNTED_DIR
+destroy_patchburner_if_exists() {
+	rm -fr $MOUNTED_DIR
 }
 
-create_patchburner()
-{
-  # Copy the template to get a clean up-to-date repo.
-  cp -r $TEMPLATE_DIR $MOUNTED_DIR
+create_patchburner() {
+	# Copy the template to get a clean up-to-date repo.
+	cp -r $TEMPLATE_DIR $MOUNTED_DIR
 
-  mkdir $MOUNTED_DIR/work/patches
+	mkdir $MOUNTED_DIR/work/patches
 
-  podman build . -t cfbot-patchburner
+	podman build . -t cfbot-patchburner
 }
 
-apply_patches_in_patchburner()
-{
-  podman run --mount=type=bind,source=$PWD/$MOUNTED_DIR/work,target=/work --workdir=/work/postgresql cfbot-patchburner /usr/local/bin/apply-patches.sh
-  rm -rf $MOUNTED_DIR/work/postgresql/.git/hooks
-  rm -rf $MOUNTED_DIR/work/postgresql/.git/config
-  cp $TEMPLATE_DIR/work/postgresql/.git/config $MOUNTED_DIR/work/postgresql/.git/config
+apply_patches_in_patchburner() {
+	podman run --mount=type=bind,source=$PWD/$MOUNTED_DIR/work,target=/work --workdir=/work/postgresql cfbot-patchburner /usr/local/bin/apply-patches.sh
+	rm -rf $MOUNTED_DIR/work/postgresql/.git/hooks
+	rm -rf $MOUNTED_DIR/work/postgresql/.git/config
+	cp $TEMPLATE_DIR/work/postgresql/.git/config $MOUNTED_DIR/work/postgresql/.git/config
 }
 
 case $1 in
-  init-template)
-    init_template
-    ;;
-  create)
-    create_patchburner
+init-template)
+	init_template
 	;;
-  destroy)
-    destroy_patchburner_if_exists
-    ;;
-  apply)
-    apply_patches_in_patchburner
-    ;;
-  template-repo-path)
-    echo $TEMPLATE_DIR/work/postgresql
-    ;;
-  burner-patch-path)
-    echo $MOUNTED_DIR/work/patches
-    ;;
-  burner-repo-path)
-    echo $MOUNTED_DIR/work/postgresql
-    ;;
-  *)
-    usage
-    ;;
+create)
+	create_patchburner
+	;;
+destroy)
+	destroy_patchburner_if_exists
+	;;
+apply)
+	apply_patches_in_patchburner
+	;;
+template-repo-path)
+	echo $TEMPLATE_DIR/work/postgresql
+	;;
+burner-patch-path)
+	echo $MOUNTED_DIR/work/patches
+	;;
+burner-repo-path)
+	echo $MOUNTED_DIR/work/postgresql
+	;;
+*)
+	usage
+	;;
 esac
