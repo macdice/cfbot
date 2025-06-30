c1cecf9704d693eefab2c4002ab9d19f7c1563c5
Thomas Munro <thomas.munro@gmail.com>
2025-06-30 09:56:53 +0000

    Receive Cirrus webhook POSTs on CI status changes.
    For now this just creates cirrus-task-update entries in work_queue which
    are ignored/deleted because there is no handler yet.  Needs some
    thought, in terms of reliability and timing, since Cirrus won't call us
    again on transient failure.  So we probably still need to keep polling,
    but with some more hacking we can at least process changes quickly most
    of the time...
    
    This URL has already been registered with Cirrus, so the wsgi endpoint
    is called in production.  If changed and deployed, the code can be reloaded
    with:
    
    $ sudo supervisorctl restart cfbot_api

---
+++ cfbot_api.py
@@ -0,0 +1,48 @@
+import cfbot_config
+import cfbot_util
+import cfbot_work_queue
+import json
+
+from flask import Flask
+from flask import request
+
+# extremely primitive approach to connection re-use; need to learn more flask
+# philosophy and do better!
+conn = cfbot_util.db()
+def error_cleanup():
+    try:
+        conn.rollback()
+    except:
+        pass
+    try:
+        conn.close()
+    except:
+        pass
+    conn = cfbot_util.db()
+
+app = Flask("cfbot_api")
+
+# This URL is registered with with cirrus so it calls us any time a build
+# begins or a status changes:
+#
+# https://cirrus-ci.org/api/#builds-and-tasks-webhooks
+#
+# We extract commit_id:task_id and create a work_queue entry for later
+# processing.
+#
+# XXX Should we process the status change immediately in this transaction?
+#
+@app.route("/api/cirrus-webhook", methods=['POST'])
+def cirrus_webhook():
+    try:
+        event = request.json
+        if "build" in event and "task" in event and "changeIdInRepo" in event["build"] and "id" in event["task"]:
+            cursor = conn.cursor()
+            cfbot_work_queue.insert_work_queue(cursor, "cirrus-task-update", event["build"]["changeIdInRepo"] + ":" + str(event["task"]["id"]))
+            conn.commit()
+            return "OK"
+        else:
+            return "not understood"
+    except RuntimeError as e:
+        error_cleanup()
+        return "NOT OK: " + str(e)
